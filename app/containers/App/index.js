6f264b98ef4b306ff2e74c747c334fd7f493b050
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-03-30 09:31:53 +0800

    Remove header

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -14,7 +14,6 @@ import { Switch, Route } from 'react-router-dom'
 import HomePage from 'containers/HomePage/Loadable'
 import FeaturePage from 'containers/FeaturePage/Loadable'
 import NotFoundPage from 'containers/NotFoundPage/Loadable'
-import Header from 'components/Header'
 import Footer from 'components/Footer'
 
 const AppWrapper = styled.div`
@@ -35,7 +34,6 @@ export default function App () {
       >
         <meta name='description' content='A Oh My Home application' />
       </Helmet>
-      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/features' component={FeaturePage} />
