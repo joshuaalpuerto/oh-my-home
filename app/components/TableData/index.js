23d169f2875eb115656bd14af71a2ccfa54df280
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-03-30 11:01:23 +0800

    add width 100%

--- app/components/TableData/index.js
+++ app/components/TableData/index.js
@@ -5,16 +5,21 @@
 */
 
 import React from 'react'
+import styled from 'styled-components'
 import PropTypes from 'prop-types'
+
+const TableWrapper = styled.table`
+  width: 100%
+`
 
 class TableData extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render () {
     const { tableHeader, tableBody } = this.props
     return (
-      <table>
+      <TableWrapper>
         { tableHeader }
         { tableBody }
-      </table>
+      </TableWrapper>
     )
   }
 }
