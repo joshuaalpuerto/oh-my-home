75b79573ee48e2cf5e1407e68926437c8d2054ce
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-03-31 17:44:11 +0800

    Separeted styled for better isolation

--- app/components/Modal/index.js
+++ app/components/Modal/index.js
@@ -6,50 +6,25 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
 
 import { FormattedMessage } from 'react-intl'
 
 import Button from 'components/Button'
 
 import messages from './messages'
-
-const ModalContainer = styled.div`
-  background:rgba(0,0,0,.8);
-  display: ${(({ open }) => open ? 'flex' : 'none')};
-  height:100%;
-  justify-content: center;
-  left:0;
-  position:fixed;
-  top:0;
-  width:100%;
-  z-index:1;
-`
-const ModalWrapper = styled.div`
-  background-color: #FFF;
-  height: auto;
-  margin: auto;
-  min-width: 300px;
-  width: 60%;
-`
-
-const ModalContent = styled.div`
-  border-radius:3px;
-  display:inline-block;
-  font-weight:300;
-  min-height: 100%;
-  padding:20px;
-  position:relative;
-  width: 100%;
-`
-
-const ModalFooter = styled.div`
-  display: flex;
-  justify-content: flex-end;
-`
+import {
+  ModalContainer,
+  ModalWrapper,
+  ModalContent,
+  ModalFooter,
+} from './styled'
 
 class Modal extends React.Component {
   static propTypes = {
+    children: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.object
+    ]).isRequired,
     open: PropTypes.bool.isRequired,
     handleToggle: PropTypes.func.isRequired
   }
