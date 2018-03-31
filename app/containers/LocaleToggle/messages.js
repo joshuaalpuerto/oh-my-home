c4f53d93d0f4bb646e4e687b8195b201a16e980c
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-03-31 18:35:30 +0800

    Update locale

--- app/containers/LocaleToggle/messages.js
+++ app/containers/LocaleToggle/messages.js
@@ -10,8 +10,8 @@ export default defineMessages({
     id: 'boilerplate.containers.LocaleToggle.en',
     defaultMessage: 'en'
   },
-  de: {
-    id: 'boilerplate.containers.LocaleToggle.de',
-    defaultMessage: 'de'
+  ph: {
+    id: 'boilerplate.containers.LocaleToggle.ph',
+    defaultMessage: 'ph'
   }
 })
