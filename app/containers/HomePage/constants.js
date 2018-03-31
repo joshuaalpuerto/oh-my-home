d282a921ec3d9076425a45e22ca14f992af9d190
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-03-31 19:10:12 +0800

    Added filter values

--- app/containers/HomePage/constants.js
+++ app/containers/HomePage/constants.js
@@ -6,3 +6,8 @@ export const GET_USERS = 'boilerplate/Home/GET_USERS'
 export const SET_USERS = 'boilerplate/Home/SET_USERS'
 
 export const TOGGLE_STATUS_USER = 'boilerplate/Home/TOGGLE_STATUS_USER'
+export const FILTERS = [
+  'all',
+  'active',
+  'inActive'
+]
