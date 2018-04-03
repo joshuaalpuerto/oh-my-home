9bc067f7ebe4511a25d2dd4de11629974f9f9d70
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-04-03 22:25:30 +0800

    updated color scheme

--- app/components/Button/buttonStyles.js
+++ app/components/Button/buttonStyles.js
@@ -14,11 +14,11 @@ const buttonStyles = css`
   font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
   font-weight: bold;
   font-size: 16px;
-  border: 2px solid #41addd;
-  color: #41addd;
+  border: 2px solid #ee620f;
+  color: #ee620f;
 
   &:active {
-    background: #41addd;
+    background: #ee620f;
     color: #fff;
   }
 `
