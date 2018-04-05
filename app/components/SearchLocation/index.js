bb7b72c6f7e2ec6a718418d35356dfd46ed20e4f
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-04-05 22:54:33 +0800

    Handling inner changes

--- app/components/SearchLocation/index.js
+++ app/components/SearchLocation/index.js
@@ -5,6 +5,7 @@
  */
 
 import React from 'react'
+import PropTypes from 'prop-types'
 import { FormattedMessage, injectIntl, intlShape } from 'react-intl'
 import { Select, Row, Col } from 'antd'
 
@@ -17,13 +18,24 @@ const Option = Select.Option
 
 export class SearchLocation extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   state = {
-    flatType: ''
+    flatType: '',
+    location: {}
   }
 
-  _handleFlatChange = (value) => {
+  _handleLocationChange = (location) => {
     this.setState({
-      flatType: value
+      location
     })
+  }
+
+  _handleFlatChange = (flatType) => {
+    this.setState({
+      flatType
+    })
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this.props.onUpdate(this.state)
   }
 
   render () {
@@ -31,7 +43,7 @@ export class SearchLocation extends React.PureComponent { // eslint-disable-line
     return (
       <Row gutter={16} type='flex' justify='center' align='middle'>
         <Col xs={24} sm={24} md={12} lg={12} xl={12}>
-          <Autocomplete />
+          <Autocomplete onUpdate={this._handleLocationChange}/>
         </Col>
         <Col xs={24} sm={24} md={4} lg={4} xl={4}>
           <Select
@@ -58,6 +70,7 @@ export class SearchLocation extends React.PureComponent { // eslint-disable-line
 }
 
 SearchLocation.propTypes = {
+  onUpdate: PropTypes.func.isRequired,
   intl: intlShape.isRequired
 }
 
