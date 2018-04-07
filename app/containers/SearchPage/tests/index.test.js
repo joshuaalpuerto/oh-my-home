4adad0d9a02b284d750435a4351dbc4b2e040c24
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-04-07 20:20:07 +0800

    Update search page tests

--- app/containers/SearchPage/tests/index.test.js
+++ app/containers/SearchPage/tests/index.test.js
@@ -4,8 +4,9 @@
 
 import React from 'react'
 import { shallow } from 'enzyme'
+import { push } from 'react-router-redux'
 
-import { SearchPage } from '../index'
+import { SearchPage, mapDispatchToProps } from '../index'
 
 const children = (<h1>Test</h1>)
 const wrapper = (props = {}, enzyme = shallow) => enzyme(
@@ -37,12 +38,39 @@ describe('<SearchPage />', () => {
     ).toEqual(1)
   })
 
-  // it('should handle filter active', () => {
-  //   const renderComponent = wrapper(minProps)
-  //   const component = renderComponent.instance()
-  //   component._handleFilter({ target: { value: 'active' } })
-  //   expect(
-  //     component.state.filter
-  //   ).toEqual('active')
-  // })
+  it('should trigger changeRoute', () => {
+    const props = {
+      ...minProps,
+      changeRoute: jest.fn()
+    }
+    const renderComponent = wrapper(props)
+    const component = renderComponent.instance()
+    component._handleSearchUpdate({
+      flatType: 'room2',
+      location: {
+        value: 'value',
+        description: 'description'
+      }
+    })
+
+    expect(
+      component.props.changeRoute
+    ).toBeCalled()
+  })
+
+  describe('changeRoute', () => {
+    it('should be injected', () => {
+      const dispatch = jest.fn()
+      const result = mapDispatchToProps(dispatch)
+      expect(result.changeRoute).toBeDefined()
+    })
+
+    it('should dispatch changeRoute when called', () => {
+      const url = '/'
+      const dispatch = jest.fn()
+      const result = mapDispatchToProps(dispatch)
+      result.changeRoute(url)
+      expect(dispatch).toHaveBeenCalledWith(push(url))
+    })
+  })
 })
