4a8ea4a090eb81086a681814baf7a21b6169884a
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-04-07 15:01:11 +0800

    Added photo base url

--- app/containers/MapPage/constants.js
+++ app/containers/MapPage/constants.js
@@ -8,3 +8,4 @@ export const GET_PLACE = 'app/MapPage/GET_PLACE'
 export const SET_PLACE = 'app/MapPage/SET_PLACE'
 
 export const MAP_KEY = 'AIzaSyDDs-19AX317p7-U-ZNGv19NQtC1JO29Ag'
+export const MAP_PHOTO_URL = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference'
