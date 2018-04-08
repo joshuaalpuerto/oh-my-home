03068ebbdd75f48785198b019bec4ffc3fd92599
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-04-08 12:45:18 +0800

    Updated test googlemaps

--- app/containers/MapPage/tests/GoogleMap.test.js
+++ app/containers/MapPage/tests/GoogleMap.test.js
@@ -3,10 +3,14 @@
  */
 
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
+import { InfoWindow } from 'react-google-maps'
 
-import GoogleMap from '../GoogleMap'
+import Img from 'components/Img'
+
+import GoogleMap, { MapComponent } from '../GoogleMap'
 import {
+  MAP_PHOTO_URL,
   MAP_KEY
 } from '../constants'
 
@@ -31,5 +35,59 @@ describe('<GoogleMap />', () => {
     expect(
       renderComponent.length
     ).toEqual(1)
+  })
+
+  describe('Testing Without DOM', () => {
+    const props = {
+      ...minProps,
+      images: [`${MAP_PHOTO_URL}=1234567&key=${MAP_KEY}`]
+    }
+    const renderComponent = shallow(<MapComponent {...props} />)
+    const component = renderComponent.instance()
+
+    it('should match snapshot', () => {
+      expect(
+        renderComponent
+      ).toMatchSnapshot()
+    })
+
+    describe('toggle infoWindow', () => {
+      component._infoToggle()
+
+      it('should equals true open state', () => {
+        expect(
+          component.state.open
+        ).toEqual(true)
+      })
+
+      it('should have infoWindow defined', () => {
+        expect(
+          renderComponent.find(InfoWindow).length
+        ).toEqual(1)
+      })
+
+      it('should have Img defined', () => {
+        expect(
+          renderComponent.find(Img).length
+        ).toEqual(1)
+      })
+    })
+  })
+
+  describe('toggle InfoMarker', () => {
+    const props = {
+      ...minProps,
+      images: [`${MAP_PHOTO_URL}=1234567&key=${MAP_KEY}`]
+    }
+    const renderComponent = wrapper(props, mount)
+    renderComponent.setState({
+      open: true
+    })
+
+    it('should have open state true', () => {
+      expect(
+        renderComponent.state().open
+      ).toEqual(true)
+    })
   })
 })
