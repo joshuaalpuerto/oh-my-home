86cd9d58c226caae0d08bf0f75117b638efec5ca
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-04-08 13:57:25 +0800

    wire up redux setup

--- app/containers/Autocomplete/actions.js
+++ app/containers/Autocomplete/actions.js
@@ -6,7 +6,11 @@
 
 import {
   GET_AUTOCOMPLETE,
-  SET_AUTOCOMPLETE
+  SET_AUTOCOMPLETE,
+
+  GET_RECENT_SEARCH,
+  SET_RECENT_SEARCH,
+  UPDATE_RECENT_SEARCH
 } from './constants'
 
 export function getAutoCompleteAction (payload) {
@@ -19,6 +23,27 @@ export function getAutoCompleteAction (payload) {
 export function setAutoCompleteAction (payload) {
   return {
     type: SET_AUTOCOMPLETE,
+    payload
+  }
+}
+
+export function getRecentSearchAction (payload) {
+  return {
+    type: GET_RECENT_SEARCH,
+    payload
+  }
+}
+
+export function setRecentSearchAction (payload) {
+  return {
+    type: SET_RECENT_SEARCH,
+    payload
+  }
+}
+
+export function updateRecentSearchAction (payload) {
+  return {
+    type: UPDATE_RECENT_SEARCH,
     payload
   }
 }
--- app/containers/Autocomplete/constants.js
+++ app/containers/Autocomplete/constants.js
@@ -6,3 +6,7 @@
 
 export const GET_AUTOCOMPLETE = 'app/Autocomplete/GET_AUTOCOMPLETE'
 export const SET_AUTOCOMPLETE = 'app/Autocomplete/SET_AUTOCOMPLETE'
+
+export const GET_RECENT_SEARCH = 'app/Autocomplete/GET_RECENT_SEARCH'
+export const SET_RECENT_SEARCH = 'app/Autocomplete/SET_RECENT_SEARCH'
+export const UPDATE_RECENT_SEARCH = 'app/Autocomplete/UPDATE_RECENT_SEARCH'
